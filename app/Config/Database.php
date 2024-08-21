ed95b9d2f0f200e59362d39a6932668814e5d823
Alef C S Santos <alef.cristian.simeao.santos@gmail.com>
2024-08-21 23:31:11 +0000

    init commit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'u663757844_ideabit',
+        'password'     => 'wM=j>H:5j*K',
+        'database'     => 'u663757844_ideabit_dash',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
