78fb25df702456fc09db020b34a659386bf8aace
Alef Santos <alefcr@hotmail.com>
2025-05-31 13:26:12 -0300

    limpando os arquivos do projeto que peguei de base

--- app/Views/templates/especialidades.php
+++ app/Views/templates/especialidades.php
@@ -1,7 +1,7 @@
  <div class="row">
      <div class="form-group col-3">
          <label>Especialidade</label>
-         <select id="guiaReferenciaEspecialidade" class="form-control select2" style="width: 100%;">
+         <select id="especialidade" class="form-control select2" style="width: 100%;">
              <option value="Ortopedia">Ortopedia</option>
              <option value="California">California</option>
              <option value="Delaware">Delaware</option>
